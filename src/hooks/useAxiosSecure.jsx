7433cd9c44a07b7eff0607205cfe2c69ed3d0488
AKnaaz <nazatakter@19gmail.com>
2025-07-17 19:52:31 +0600

    update useAxiosSecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,18 +1,42 @@
 import axios from 'axios';
 import useAuth from './useAuth';
-
-const axiosSecure = axios.create({
-  baseURL: `http://localhost:3000`
-});
+import { useNavigate } from 'react-router';
 
 const useAxiosSecure = () => {
-  const { user } = useAuth();
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
+
+  const axiosSecure = axios.create({
+    baseURL: `http://localhost:3000`
+  });
+
+
   axiosSecure.interceptors.request.use(config => {
-    config.headers.Authorization = `Bearer ${user.accessToken}`
+    if (user?.accessToken) {
+      config.headers.Authorization = `Bearer ${user.accessToken}`;
+    }
     return config;
   }, error => {
     return Promise.reject(error);
-  })
+  });
+
+  // axiosSecure.interceptors.response.use(response => {
+  //   return response;
+  // }, error => {
+  //   const status = error.response?.status;
+  //   if (status === 403) {
+  //     navigate("/");
+  //   }
+  //   else if (status === 401) {
+  //     logOut()
+  //     .then(() => {
+  //       navigate("/")
+  //     })
+  //     .catch(() => { })
+  //   }
+  //   return Promise.reject(error);
+  // });
+
   return axiosSecure;
 };
 
